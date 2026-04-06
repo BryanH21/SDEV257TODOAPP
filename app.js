6e99aeea50d425f74adc16bc6b64587d2890d769
BryanH21 <bryanhernan1019@gmail.com>
2026-04-06 14:06:57 -0400

    Fix task bugs in App.js

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
     return (
-        <View>
-            <Text>Todo List</Text>
+        <View style={styles.container}>
+            
+            {/* Today's Tasks */}
+            <View style={styles.tasksWrapper}>
+                <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+                <View style={styles.items}>
+                    {/* This is where the tasks will go */}
+                    <Task text={'Task1'} />
+                    <Task />
+                </View>
+
+            </View>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#E8EAED',
+    },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    items: {
+        marginTop: 30,
+    },
+});
